e367db25dc4fc77fb213a841f8df9656a7df507a
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-04-07 08:03:42 -0400

    initial commit of example action cable app - TODO: adapt to mesh network

---
+++ app/channels/application_cable/channel.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+module ApplicationCable
+  class Channel < ActionCable::Channel::Base
+    # identified_by :uuid
+    #
+    # def connect
+    #   self.uuid = SecureRandom.uuid
+    # end
+  end
+end
---
+++ app/channels/room_channel.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    # ActionCable.server.broadcast "room_channel", message: data['message']
+    Message.create! content: data['message']
+  end
+end
